8b8f0a274514d34de075c1c90ec9bf0c0f33ac65
Dennis <dennis@alexandersson.xyz>
2017-09-22 16:16:21 +0200

    Updated archive.sh

--- archive.sh
+++ archive.sh
@@ -5,8 +5,8 @@ RELEASE_DIR=$(mktemp -d)
 
 VERSION=$(cat ${WORKSPACE_DIR}/build.gradle | grep "version '" | awk '{print $2}' | tr -d \')
 
-mkdir ${RELEASE_DIR}/WorkLog
-cp ${WORKSPACE_DIR}/build/libs/WorkLog-*-all.jar ${RELEASE_DIR}/WorkLog/WorkLog.jar
+mkdir "${RELEASE_DIR}/WorkLog ${VERSION}"
+cp ${WORKSPACE_DIR}/build/libs/WorkLog-*-all.jar "${RELEASE_DIR}/WorkLog ${VERSION}/WorkLog.jar"
 cp ${WORKSPACE_DIR}/ReleaseFiles/* ${RELEASE_DIR}
 
 cd ${RELEASE_DIR}
