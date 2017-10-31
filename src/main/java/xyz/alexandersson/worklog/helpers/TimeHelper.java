54f2a2cb9a60ca01f2daff1a6f7a1d1a157aa716
Dennis <dennis@alexandersson.xyz>
2017-10-31 20:33:42 +0100

    Fixed non-decimal time calculation being floored instead of rounded WL-36

--- src/main/java/xyz/alexandersson/worklog/helpers/TimeHelper.java
+++ src/main/java/xyz/alexandersson/worklog/helpers/TimeHelper.java
@@ -3,7 +3,7 @@ package xyz.alexandersson.worklog.helpers;
 public class TimeHelper {
     public static String hourDecimalToString(Double time) {
         int hours = (int) Math.floor(time);
-        int minutes = (int) Math.floor((time * 60) % 60);
+        int minutes = (int) Math.round((time * 60) % 60);
 
         return String.format("%dh %dm", hours, minutes);
     }
