95beaa0cfa6f993f37f3db5345b2eef5b992c219
Dennis Alexandersson <dennis@alexandersson.xyz>
2020-03-18 21:10:47 +0100

    Update travis.yml

--- archive.sh
+++ archive.sh
@@ -10,7 +10,7 @@ cp ${WORKSPACE_DIR}/build/libs/WorkLog.jar "${RELEASE_DIR}/WorkLog ${VERSION}/"
 cp ${WORKSPACE_DIR}/ReleaseFiles/* ${RELEASE_DIR}
 
 cd ${RELEASE_DIR}
-zip -r ${WORKSPACE_DIR}/build/WorkLog-${VERSION}-${BUILD_NUMBER}.zip .
+zip -r ${WORKSPACE_DIR}/build/WorkLog-${VERSION}-${TRAVIS_BUILD_NUMBER}.zip .
 
 cd ${WORKSPACE_DIR}
 
